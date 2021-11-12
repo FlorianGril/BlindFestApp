fdff91c13fcfbe808868f9a14dda0bf4cecd5ca1
Nicolas Poupon <Nicolas.Poupon.Etu@univ-lemans.fr>
2021-11-12 00:30:34 -0500

    Radio bouton et toast
    Rien ne marche, gl à nous

--- app/src/main/java/com/example/blindfest/MainActivity.kt
+++ app/src/main/java/com/example/blindfest/MainActivity.kt
@@ -3,14 +3,47 @@ package com.example.blindfest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 
     fun jouer(view: View){
-        setContentView(R.layout.activity_regle);
+        val radio: RadioButton = findViewById(R.id.radioButton2)
+
+        Toast.makeText(applicationContext,"On click : ${radio.text}", Toast.LENGTH_LONG).show()
+
+        Toast.makeText(this@MainActivity,"MARC", Toast.LENGTH_LONG).show()
+
+        //setContentView(R.layout.activity_regle);
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.radioButton2 ->
+                    if (checked) {
+                        Toast.makeText(this,"MARC", Toast.LENGTH_LONG).show()
+                    }
+                R.id.radioButton3 ->
+                    if (checked) {
+                        // Ninjas rule
+                    }
+                R.id.radioButton4 ->
+                    if (checked) {
+                        // Ninjas rule
+                    }
+            }
+        }
     }
 }
